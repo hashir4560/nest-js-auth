123ec181785e0254f98141b853c16c8573c7caf2
hashir4560 <haashirraja@gmail.com>
2023-11-16 13:31:36 +0500

    Update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
-import { arrayBuffer } from 'stream/consumers';
 
 @Controller('auth')
 export class AuthController {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,46 @@
+// import { Injectable, UnauthorizedException } from '@nestjs/common';
+// // import users from '../users.json';
+// const users = require('../users.json') as { email: string; password: string }[];
+// import { AuthDto } from './dto';
+
+// @Injectable()
+// export class AuthService {
+//   signinLocal(dto: AuthDto) {
+//     //Retrieve the user
+
+//     const user = users.find((_user) => _user.email === dto.email);
+//     if (!user) throw new UnauthorizedException('Credentials incorrect  ');
+//     if (user.password !== dto.password)
+//       throw new UnauthorizedException('Credentials incorrect');
+
+//     return user;
+//   }
+//   signupLocal(dto: AuthDto) {}
+// }
+<<<<<<< Updated upstream
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 // import users from '../users.json';
-const users = require('../users.json') as { email: string; password: string }[];
+const users = require('../users.json');
+=======
+
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as path from 'path';
+const usersPath = path.resolve(__dirname, '../users.json');
+const users = require(usersPath) as { email: string; password: string }[];
+>>>>>>> Stashed changes
 import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   signinLocal(dto: AuthDto) {
-    //Retrieve the user
-
     const user = users.find((_user) => _user.email === dto.email);
-    if (!user) throw new UnauthorizedException('Credentials incorrect  ');
+    if (!user) throw new UnauthorizedException('Credentials incorrect ');
     if (user.password !== dto.password)
       throw new UnauthorizedException('Credentials incorrect');
-
     return user;
   }
-  signupLocal(dto: AuthDto) {}
+
+  signupLocal(dto: AuthDto) {
+    // Implement signup logic if needed
+  }
 }
