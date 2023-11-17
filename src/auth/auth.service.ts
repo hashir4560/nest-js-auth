93c8fcd36bbf0431a63be87363900f9cfb828bac
hashir4560 <haashirraja@gmail.com>
2023-11-17 12:23:03 +0500

    Jwt Token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,8 @@
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-// // import users from '../users.json';
-// const users = require('../users.json') as { email: string; password: string }[];
-// import { AuthDto } from './dto';
-
-// @Injectable()
-// export class AuthService {
-//   signinLocal(dto: AuthDto) {
-//     //Retrieve the user
-
-//     const user = users.find((_user) => _user.email === dto.email);
-//     if (!user) throw new UnauthorizedException('Credentials incorrect  ');
-//     if (user.password !== dto.password)
-//       throw new UnauthorizedException('Credentials incorrect');
-
-//     return user;
-//   }
-//   signupLocal(dto: AuthDto) {}
-// }
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 // import users from '../users.json';
 // eslint-disable-next-line
-
 const users = require('../users.json');
+
 // const users = require('../users.json');
 
 import { AuthDto } from './dto';
@@ -39,9 +20,15 @@ export class AuthService {
     if (user.password !== dto.password)
       throw new UnauthorizedException('Credentials incorrect');
 
-    return user;
+    return this.signUser(user.id, user.email, 'user');
   }
   signupLocal(dto: AuthDto) {}
 
-  signUser(userId: number, email: string, type: string) {}
+  signUser(userId: number, email: string, type: string) {
+    return this.jwtService.sign({
+      sub: userId,
+      email: email,
+      type: type,
+    });
+  }
 }
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: 'super-sceret-key',
+    });
+  }
+  async validate(payload: any) {
+    return payload;
+  }
+}
